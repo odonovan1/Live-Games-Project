86dd821e2df324ef169ad8797ee03923c7ae6a62
odonovan1 <odogdonovan@gmail.com>
2023-04-04 00:00:04 -0400

    added grid to live games

--- client/src/pages/LiveGames.jsx
+++ client/src/pages/LiveGames.jsx
@@ -14,12 +14,12 @@ const LiveGames = ({ games, user, setChosen }) => {
       {games.map((game) => (
         <div ket={game.id} className="liveGame">
           <Link to={`/details`} onClick={() => onClick(game)}>
-            <h1>{game.sport_title}</h1>
+            <h2>{game.sport_title}</h2>
             <div className="matchup">
-              <h3>{game.home_team} {game.bookmakers[0].markets[0].outcomes[0].price}</h3>
+              <h3>{game.home_team} ({game.bookmakers[0].markets[0].outcomes[0].price})</h3>
 
-              <h2>vs</h2>
-              <h3>{game.away_team} {game.bookmakers[0].markets[0].outcomes[1].price}</h3>
+              <h2 className="vs">vs</h2>
+              <h3>{game.away_team} ({game.bookmakers[0].markets[0].outcomes[1].price})</h3>
             </div>
           </Link>
         </div>
