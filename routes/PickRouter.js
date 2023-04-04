78e1353b72170002449733986e7b3aef6d432803
odonovan1 <odogdonovan@gmail.com>
2023-04-04 01:34:17 -0400

    added new route

--- routes/PickRouter.js
+++ routes/PickRouter.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/PickController')
 
 router.post('/create/:user_id/:game_id', controller.CreatePick)
 router.get('/:user_id/picks', controller.getUserPicks)
+router.delete('/:pick_id/delete', controller.deletePick)
 
 module.exports = router
