e5166191a341c4178347ff2f473a2028ca7e30ee
odonovan1 <odogdonovan@gmail.com>
2023-04-04 13:00:29 -0400

    final styling touches before presentations

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = ({ user, setUser }) => {
         <h1 className='h1'>Welcome to the future of social picking!</h1>
       </div>
       <img src='https://www.pinclipart.com/picdir/big/557-5575148_download-free-png-hd-sports-activities-clipart-sports.png' />
-      <h3 className='h1'>Checkout <NavLink to="/games" className="clickable">Live/Upcoming Games</NavLink> or take a look at what <NavLink to="/users" className="clickable">other people</NavLink> are picking!</h3>
+      <h3 className='h2'>Checkout <NavLink to="/games" className="clickable">Live/Upcoming Games</NavLink> or take a look at what <NavLink to="/users" className="clickable">other people</NavLink> are picking!</h3>
     </div>
   ) : (
     <div>
