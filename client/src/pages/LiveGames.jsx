705678c4630c520f9840fd7ada6f7dca3ec61b4e
odonovan1 <odogdonovan@gmail.com>
2023-04-04 00:20:16 -0400

    changed format of live games

--- client/src/pages/LiveGames.jsx
+++ client/src/pages/LiveGames.jsx
@@ -24,15 +24,21 @@ const LiveGames = ({ games, user, setChosen }) => {
       {games.map((game) => (
         <div ket={game.id} className="liveGame">
           <Link to={`/details`} onClick={() => onClick(game)}>
-            <h2>{game.sport_title}</h2>
+            <h2 className="title">{game.sport_title}</h2>
             <div className="matchup">
-              <h3>{game.home_team} ({game.bookmakers[0].markets[0].outcomes[0].price})</h3>
-
+              <div className="teams">
+                <h2>Home</h2>
+                <h3>{game.home_team} ({game.bookmakers[0].markets[0].outcomes[0].price})</h3>
+              </div>
               <h2 className="vs">vs</h2>
-              <h3>{game.away_team} ({game.bookmakers[0].markets[0].outcomes[1].price})</h3>
+              <div className="teams">
+                <h2>Away</h2>
+                <h3>{game.away_team} ({game.bookmakers[0].markets[0].outcomes[1].price})</h3>
+              </div>
             </div>
-            <div>
-              <h4>{handleTime(game.commence_time).slice(0, 5)}</h4>
+            <div className="date">
+              <h4>{handleDate(game.commence_time)}</h4>
+              <h4>@ {handleTime(game.commence_time).slice(0, 5)}</h4>
             </div>
           </Link>
         </div>
